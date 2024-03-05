30e08943a05786e4eee341d9ea3d64b2dd781934
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-05 06:52:44 +0200

    init project

---
+++ SwiftUI_Features/SwiftUI_Features/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 05.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI_Features/SwiftUI_Features/SwiftUI_FeaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FeaturesApp.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 05.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FeaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
