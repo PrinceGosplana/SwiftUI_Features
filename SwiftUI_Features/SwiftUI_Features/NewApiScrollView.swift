84ac7c42f00d25b9ab2182c2cad2c6bb08a5331a
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-06 11:05:41 +0200

    add paging behave

--- SwiftUI_Features/SwiftUI_Features/NewApiScrollView.swift
+++ SwiftUI_Features/SwiftUI_Features/NewApiScrollView.swift
@@ -12,14 +12,19 @@ struct NewApiScrollView: View {
         ScrollView(.horizontal) {
             let colors: [Color] = [.red, .blue, .green, .yellow, .purple]
             
-            LazyHStack(spacing: 25) {
+            LazyHStack(spacing: 0) {
                 ForEach(colors, id: \.self) { color in
                     RoundedRectangle(cornerRadius: 15)
+                        .fill(color.gradient)
                         .frame(height: 200)
+                        .padding(.horizontal)
                         .containerRelativeFrame(.horizontal)
                 }
             }
+            /// step 1
+            .scrollTargetLayout()
         }
+        .scrollTargetBehavior(.paging)
     }
 }
 
