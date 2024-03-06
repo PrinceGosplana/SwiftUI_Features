be96370e99a5210d31813a13cdc1065f843a5e29
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-06 10:56:15 +0200

    add empty view for scroll view example

---
+++ SwiftUI_Features/SwiftUI_Features/NewApiScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  NewApiScrollView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 06.03.2024.
+//
+
+import SwiftUI
+
+struct NewApiScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewApiScrollView()
+}
