6a4a943e8a82ca2f3663aef82152893212db86e7
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-06 13:36:38 +0200

    add new example

---
+++ SwiftUI_Features/SwiftUI_Features/ReadingScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  ReadingScrollView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 06.03.2024.
+//
+
+import SwiftUI
+
+struct ReadingScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReadingScrollView()
+}
