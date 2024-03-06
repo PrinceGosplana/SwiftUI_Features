919c5b5e79bb19a97e66cb01802e7f5051743245
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-06 13:56:52 +0200

    add text in bottom of scroll

--- SwiftUI_Features/SwiftUI_Features/ReadingScrollView.swift
+++ SwiftUI_Features/SwiftUI_Features/ReadingScrollView.swift
@@ -8,8 +8,35 @@
 import SwiftUI
 
 struct ReadingScrollView: View {
+    @State private var scrollPosition: Color?
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader{
+            let size = $0.size
+            
+            ScrollView(.horizontal) {
+                let colors: [Color] = [.red, .blue, .green, .yellow, .purple]
+                
+                LazyHStack(spacing: 25) {
+                    ForEach(colors, id: \.self) { color in
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(color.gradient)
+                            .frame(width: 300, height: 200)
+                    }
+                }
+                .padding(.horizontal, (size.width - 300) / 2)
+                /// step 1
+                .scrollTargetLayout()
+            }
+            .scrollTargetBehavior(.viewAligned)
+            .scrollPosition(id: $scrollPosition)
+        }
+        .frame(height: 200)
+        .overlay(alignment: .bottom) {
+            if let scrollPosition {
+                Text(scrollPosition == .red ? "Red" : "Other Colors")
+                    .offset(y: 100)
+            }
+        }
     }
 }
 
