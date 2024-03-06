be7ccb37c88d8cacc761695be8ebb45b74ce12a8
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-06 11:17:39 +0200

    left side paging

--- SwiftUI_Features/SwiftUI_Features/NewApiScrollView.swift
+++ SwiftUI_Features/SwiftUI_Features/NewApiScrollView.swift
@@ -7,6 +7,32 @@
 
 import SwiftUI
 
+// paging from left side 
+struct NewApiScrollView: View {
+    var body: some View {
+        ScrollView(.horizontal) {
+            let colors: [Color] = [.red, .blue, .green, .yellow, .purple]
+            
+            LazyHStack(spacing: 25) {
+                ForEach(colors, id: \.self) { color in
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(color.gradient)
+                        .frame(width: 300, height: 200)
+                }
+            }
+            .padding(.horizontal)
+            /// step 1
+            .scrollTargetLayout()
+        }
+        .scrollTargetBehavior(.viewAligned)
+    }
+}
+
+#Preview {
+    NewApiScrollView()
+}
+/*
+// paging from the start
 struct NewApiScrollView: View {
     var body: some View {
         ScrollView(.horizontal) {
@@ -31,3 +57,4 @@ struct NewApiScrollView: View {
 #Preview {
     NewApiScrollView()
 }
+*/
