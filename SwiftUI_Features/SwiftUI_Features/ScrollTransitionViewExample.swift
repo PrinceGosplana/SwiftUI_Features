eed374379b16198a938bf2ad2cb4f1e9b1db99b6
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-07 18:43:06 +0200

    add empty view for scroll transition

---
+++ SwiftUI_Features/SwiftUI_Features/ScrollTransitionViewExample.swift
@@ -0,0 +1,18 @@
+//
+//  ScrollTransitionViewExample.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 07.03.2024.
+//
+
+import SwiftUI
+
+struct ScrollTransitionViewExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScrollTransitionViewExample()
+}
