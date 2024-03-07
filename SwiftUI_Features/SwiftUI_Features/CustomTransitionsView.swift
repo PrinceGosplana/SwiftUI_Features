d42a06880bb971aee7796e2a1908120abb2eb3cb
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-07 18:52:10 +0200

    add view for custom transitions

---
+++ SwiftUI_Features/SwiftUI_Features/CustomTransitionsView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTransitionsView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 07.03.2024.
+//
+
+import SwiftUI
+
+struct CustomTransitionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTransitionsView()
+}
