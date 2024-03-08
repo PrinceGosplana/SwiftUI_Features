78de6a5b3c48d373d83f1788810131585e48759c
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-08 20:56:01 +0200

    add empty view for customs animations

---
+++ SwiftUI_Features/SwiftUI_Features/CustomAnnimationsView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomAnnimationsView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 08.03.2024.
+//
+
+import SwiftUI
+
+struct CustomAnnimationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomAnnimationsView()
+}
