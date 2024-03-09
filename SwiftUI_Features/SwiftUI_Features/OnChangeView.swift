49d774d45a2f0a626a8ee0c1b8fd26b479302c75
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-09 10:01:23 +0200

    add new view for example of update on change view

---
+++ SwiftUI_Features/SwiftUI_Features/OnChangeView.swift
@@ -0,0 +1,18 @@
+//
+//  OnChangeView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 09.03.2024.
+//
+
+import SwiftUI
+
+struct OnChangeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnChangeView()
+}
