b8f6adc5de5753991bc63e55a6290d0e00b6c419
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-09 10:33:06 +0200

    add 2 types of modifiers

--- SwiftUI_Features/SwiftUI_Features/UnEvenRoundedRectangleView.swift
+++ SwiftUI_Features/SwiftUI_Features/UnEvenRoundedRectangleView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct UnEvenRoundedRectangleView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            UnevenRoundedRectangle(topLeadingRadius: 35, bottomLeadingRadius: 15, bottomTrailingRadius: 35, topTrailingRadius: 15, style: .circular)
+                .fill(.red.gradient)
+                .frame(width: 200, height: 200)
+            
+            Rectangle()
+                .fill(.black.gradient)
+                .frame(width: 200, height: 200)
+                .clipShape(.rect(topLeadingRadius: 35))
+        }
     }
 }
 
