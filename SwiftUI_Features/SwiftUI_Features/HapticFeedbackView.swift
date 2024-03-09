2bf87970c28f37b66908482237c390514513b07f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-09 10:34:38 +0200

    add empty haptic feedback view

---
+++ SwiftUI_Features/SwiftUI_Features/HapticFeedbackView.swift
@@ -0,0 +1,18 @@
+//
+//  HapticFeedbackView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 09.03.2024.
+//
+
+import SwiftUI
+
+struct HapticFeedbackView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HapticFeedbackView()
+}
