b1279ebafc31bd9aa2b00b977ed627c42d4df724
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-09 10:26:35 +0200

    add binding model with textfield

--- SwiftUI_Features/SwiftUI_Features/ObservBindableView.swift
+++ SwiftUI_Features/SwiftUI_Features/ObservBindableView.swift
@@ -5,15 +5,27 @@
 //  Created by OLEKSANDR ISAIEV on 09.03.2024.
 //
 
+import SwiftData
 import SwiftUI
 
 /// A new way to create an observable object in SwiftUI
 struct ObservBindableView: View {
+    @Bindable var user: User = .init()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TextField("Name", text: $user.name)
+        }
+        .padding()
     }
 }
 
 #Preview {
     ObservBindableView()
+}
+
+@Observable
+class User {
+    var name: String = ""
+    var email: String = ""
 }
