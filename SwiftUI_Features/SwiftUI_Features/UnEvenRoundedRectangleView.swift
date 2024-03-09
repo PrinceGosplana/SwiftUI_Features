ece39491ef05a00fb9d6532ffeec2e588e8996ad
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-09 10:27:29 +0200

    add empty view for unevenroundedrectangle

---
+++ SwiftUI_Features/SwiftUI_Features/UnEvenRoundedRectangleView.swift
@@ -0,0 +1,18 @@
+//
+//  UnEvenRoundedRectangleView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 09.03.2024.
+//
+
+import SwiftUI
+
+struct UnEvenRoundedRectangleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UnEvenRoundedRectangleView()
+}
