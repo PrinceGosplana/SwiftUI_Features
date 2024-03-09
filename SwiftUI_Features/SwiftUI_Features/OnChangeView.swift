f27e29a204b0838dd2b3f570223c9b4843e0dd4f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-09 10:20:00 +0200

    add example of onChange

--- SwiftUI_Features/SwiftUI_Features/OnChangeView.swift
+++ SwiftUI_Features/SwiftUI_Features/OnChangeView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct OnChangeView: View {
+    @State private var value: CGFloat = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+         
+            Button("Show View") {
+                value = 20
+            }
+            /// Now you can initially fire the onChange modifier, and it will be fired when the view is first visible, thus eliminating the use of the onAppear modifier. Also, the onChange  modifier is giving the old and new values as well
+            .onChange(of: value, initial: true) { oldValue, newValue in
+                print(oldValue, newValue)
+            }
+        }
     }
 }
 
