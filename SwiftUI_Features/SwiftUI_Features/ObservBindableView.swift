59f01d0b7b87f49cb919ae90731c270acf9bdd7f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-09 10:21:44 +0200

    add view for observable and bindable

---
+++ SwiftUI_Features/SwiftUI_Features/ObservBindableView.swift
@@ -0,0 +1,19 @@
+//
+//  ObservBindableView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 09.03.2024.
+//
+
+import SwiftUI
+
+/// A new way to create an observable object in SwiftUI
+struct ObservBindableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObservBindableView()
+}
