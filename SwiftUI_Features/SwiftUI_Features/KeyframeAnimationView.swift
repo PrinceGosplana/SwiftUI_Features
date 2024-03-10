4021e5f7ed1c3244c363d6e44fc7fe41d51ce3ad
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-10 14:01:58 +0200

    prepare animation

--- SwiftUI_Features/SwiftUI_Features/KeyframeAnimationView.swift
+++ SwiftUI_Features/SwiftUI_Features/KeyframeAnimationView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct KeyframeAnimationView: View {
+    @State private var startKeyframeAnimation: Bool = false
     var body: some View {
         VStack {
+            Spacer()
+            
             Image(.xcode)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+            Spacer()
+            
+            Button("Keyframe animation") {
+                startKeyframeAnimation.toggle()
+            }
+            .fontWeight(.bold)
+            
         }
     }
 }
