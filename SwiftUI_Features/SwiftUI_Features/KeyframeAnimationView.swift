0883ed87bf4e101598bd01920f131bc08ff98e65
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-10 13:53:56 +0200

    add new view and image for keyframe animation

---
+++ SwiftUI_Features/SwiftUI_Features/KeyframeAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  KeyframeAnimationView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 10.03.2024.
+//
+
+import SwiftUI
+
+struct KeyframeAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KeyframeAnimationView()
+}
