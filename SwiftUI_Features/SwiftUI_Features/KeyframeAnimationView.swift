2f5b60fe5cec9d82ead25e95d05137a31b1d4438
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-10 13:56:52 +0200

    change icon

--- SwiftUI_Features/SwiftUI_Features/KeyframeAnimationView.swift
+++ SwiftUI_Features/SwiftUI_Features/KeyframeAnimationView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct KeyframeAnimationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(.xcode)
+        }
     }
 }
 
