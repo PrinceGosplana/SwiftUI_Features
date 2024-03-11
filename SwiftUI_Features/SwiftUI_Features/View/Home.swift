96e4770a55d1b1d3c771e2f42537aae2968a9ef9
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-11 18:55:03 +0200

    add new view for example

---
+++ SwiftUI_Features/SwiftUI_Features/View/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 11.03.2024.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
