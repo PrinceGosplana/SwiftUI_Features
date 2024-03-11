18e49a92482992ee5339e0028e11db91f324023c
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-11 18:52:23 +0200

    add examples

--- SwiftUI_Features/SwiftUI_Features/Model/Intro.swift
+++ SwiftUI_Features/SwiftUI_Features/Model/Intro.swift
@@ -17,3 +17,37 @@ struct Intro: Identifiable {
     var circleOffset: CGFloat = 0
     var textOffset: CGFloat = 0
 }
+
+/// Sample Intros
+var sampleIntros: [Intro] = [
+    .init(
+        text: "Let's Create",
+        textColor: .color4,
+        circleColor: .color4,
+        bgColor: .color1
+    ),
+    .init(
+        text: "Let's Brain Storm",
+        textColor: .color1,
+        circleColor: .color1,
+        bgColor: .color2
+    ),
+    .init(
+        text: "Let's Explore",
+        textColor: .color2,
+        circleColor: .color2,
+        bgColor: .color3
+    ),
+    .init(
+        text: "Let's Invent",
+        textColor: .color3,
+        circleColor: .color3,
+        bgColor: .color4
+    ),
+    .init(
+        text: "Let's Create",
+        textColor: .color4,
+        circleColor: .color4,
+        bgColor: .color1
+    )
+]
