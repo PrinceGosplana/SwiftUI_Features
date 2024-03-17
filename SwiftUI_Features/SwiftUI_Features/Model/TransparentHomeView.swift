5db3a4c00db58dff89866e4a3134863772bb2f08
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-17 12:07:54 +0200

    add transparent home empty view

---
+++ SwiftUI_Features/SwiftUI_Features/Model/TransparentHomeView.swift
@@ -0,0 +1,18 @@
+//
+//  TransparentHomeView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 17.03.2024.
+//
+
+import SwiftUI
+
+struct TransparentHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransparentHomeView()
+}
