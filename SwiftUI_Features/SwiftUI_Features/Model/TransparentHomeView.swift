9ed49f5a721dacc193872b5b156c39ca4b952e20
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-17 12:23:55 +0200

    add text on screen with type of blur message

--- SwiftUI_Features/SwiftUI_Features/Model/TransparentHomeView.swift
+++ SwiftUI_Features/SwiftUI_Features/Model/TransparentHomeView.swift
@@ -10,20 +10,27 @@ import SwiftUI
 struct TransparentHomeView: View {
     /// View Properties
     @State private var activePic: String = "piggy"
+    @State private var blurType: BlurType = .freeStyle
     
     var body: some View {
         ScrollView(.vertical) {
             VStack(spacing: 15) {
-                GeometryReader {
-                    let size = $0.size
+                VStack(alignment: .leading, spacing: 10, content: {
+                    GeometryReader {
+                        let size = $0.size
+                        
+                        Image(activePic)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: size.width, height: size.height)
+                            .clipShape(.rect(cornerRadius: 25))
+                    }
+                    .frame(height: 500)
                     
-                    Image(activePic)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: size.width, height: size.height)
-                        .clipShape(.rect(cornerRadius: 25))
-                }
-                .frame(height: 500)
+                    Text("Blur Type")
+                        .font(.caption)
+                        .foregroundStyle(.gray)
+                })
                 .padding(15)
             }
         }
