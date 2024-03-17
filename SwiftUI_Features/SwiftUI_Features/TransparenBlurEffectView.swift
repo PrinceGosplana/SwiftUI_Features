bb1e211cd5a3dd359b5443cf04561c1eb8f10d86
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-17 12:05:48 +0200

    add new empty view for transparent effect

---
+++ SwiftUI_Features/SwiftUI_Features/TransparenBlurEffectView.swift
@@ -0,0 +1,18 @@
+//
+//  TransparenBlurEffectView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 17.03.2024.
+//
+
+import SwiftUI
+
+struct TransparenBlurEffectView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransparenBlurEffectView()
+}
