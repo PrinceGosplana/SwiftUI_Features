2f81dd6575e7de2162683e4b28f954ad781521c6
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-18 20:25:19 +0200

    add new file

---
+++ SwiftUI_Features/SwiftUI_Features/Model/TransparentBlurView.swift
@@ -0,0 +1,18 @@
+//
+//  TransparentBlurView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 18.03.2024.
+//
+
+import SwiftUI
+
+struct TransparentBlurView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransparentBlurView()
+}
