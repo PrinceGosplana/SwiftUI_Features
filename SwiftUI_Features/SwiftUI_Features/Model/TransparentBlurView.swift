93ee9a0bbd8961c6bd0af28f8c6cb7cc8e6da4b3
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-18 20:35:53 +0200

    remove all filters except one

--- SwiftUI_Features/SwiftUI_Features/Model/TransparentBlurView.swift
+++ SwiftUI_Features/SwiftUI_Features/Model/TransparentBlurView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct TransparentBlurView: UIViewRepresentable {
+    var removeAllFilters: Bool = false
+    
     func makeUIView(context: Context) -> UIVisualEffectView {
         let view = UIVisualEffectView(effect: UIBlurEffect(style: .systemUltraThinMaterial))
         
@@ -17,7 +19,14 @@ struct TransparentBlurView: UIViewRepresentable {
     func updateUIView(_ uiView: UIViewType, context: Context) {
         DispatchQueue.main.async {
             if let backdropLayer = uiView.layer.sublayers?.first {
-                
+                if removeAllFilters {
+                    backdropLayer.filters = []
+                } else {
+                    /// Removing all expect blur filter
+                    backdropLayer.filters?.removeAll(where: { filter in
+                        String(describing: filter) != "gaussianBlur"
+                    })
+                }
             }
         }
     }
