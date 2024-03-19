657af4f238f66112b6512aa03356fa0419d84cd1
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-19 09:43:51 +0200

    add home view for glass project

---
+++ SwiftUI_Features/SwiftUI_Features/Glass Background Effect/HomeGlassView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeGlassView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 19.03.2024.
+//
+
+import SwiftUI
+
+struct HomeGlassView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeGlassView()
+}
