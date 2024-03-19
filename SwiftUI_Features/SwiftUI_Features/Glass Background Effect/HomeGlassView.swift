b384e8c1e26e36d433d99d7e6e7dd2cceca29faf
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-19 09:53:46 +0200

    add several elements on view

--- SwiftUI_Features/SwiftUI_Features/Glass Background Effect/HomeGlassView.swift
+++ SwiftUI_Features/SwiftUI_Features/Glass Background Effect/HomeGlassView.swift
@@ -10,7 +10,16 @@ import SwiftUI
 struct HomeGlassView: View {
     var body: some View {
         VStack {
-            
+            /// Login View
+            VStack(spacing: 12) {
+                Text("Welcome!")
+                    .font(.title.bold())
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Username")
+                        .font(.callout.bold())
+                }
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background {
