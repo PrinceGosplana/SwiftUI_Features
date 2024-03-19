800472589d93d832a3fe8a129b27378539b72ce6
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2024-03-19 09:25:23 +0200

    add geometry reader

--- SwiftUI_Features/SwiftUI_Features/Model/TransparentHomeView.swift
+++ SwiftUI_Features/SwiftUI_Features/Model/TransparentHomeView.swift
@@ -13,47 +13,52 @@ struct TransparentHomeView: View {
     @State private var blurType: BlurType = .freeStyle
     
     var body: some View {
-        ScrollView(.vertical) {
-            VStack(spacing: 15) {
-                TransparentBlurView(removeAllFilters: false)
-                    .frame(height: 140)
-                    .visualEffect { view, proxy in
-                        /// Available from iOS 17 to read scroll offsets directly from SwiftUI view's
-                        view
-                            .offset(y: (proxy.bounds(of: .scrollView)?.minY ?? 0))
-                    }
-                /// Placing it above all the views
-                    .zIndex(1000)
-                
-                VStack(alignment: .leading, spacing: 10, content: {
-                    GeometryReader {
-                        let size = $0.size
-                        
-                        Image(activePic)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: size.width, height: size.height)
-                            .clipShape(.rect(cornerRadius: 25))
-                    }
-                    .frame(height: 500)
-                    
-                    Text("Blur Type")
-                        .font(.caption)
-                        .foregroundStyle(.gray)
-                        .padding(.top, 15)
-                    
-                    Picker("", selection: $blurType) {
-                        ForEach(BlurType.allCases, id: \.self) { type in
-                            Text(type.rawValue)
-                                .tag(type)
+        GeometryReader {
+            let safeArea = $0.safeAreaInsets
+            
+            ScrollView(.vertical) {
+                VStack(spacing: 15) {
+                    TransparentBlurView(removeAllFilters: false)
+                        .frame(height: 100 + safeArea.top)
+                        .visualEffect { view, proxy in
+                            /// Available from iOS 17 to read scroll offsets directly from SwiftUI view's
+                            view
+                                .offset(y: (proxy.bounds(of: .scrollView)?.minY ?? 0))
                         }
-                    }
-                    .pickerStyle(.segmented)
-                })
-                .padding(15)
+                    /// Placing it above all the views
+                        .zIndex(1000)
+                    
+                    VStack(alignment: .leading, spacing: 10, content: {
+                        GeometryReader {
+                            let size = $0.size
+                            
+                            Image(activePic)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: size.width, height: size.height)
+                                .clipShape(.rect(cornerRadius: 25))
+                        }
+                        .frame(height: 500)
+                        
+                        Text("Blur Type")
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                            .padding(.top, 15)
+                        
+                        Picker("", selection: $blurType) {
+                            ForEach(BlurType.allCases, id: \.self) { type in
+                                Text(type.rawValue)
+                                    .tag(type)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    })
+                    .padding(15)
+                }
             }
+            .scrollIndicators(.hidden)
+            .ignoresSafeArea(.container, edges: .top)
         }
-        .scrollIndicators(.hidden)
     }
 }
 
