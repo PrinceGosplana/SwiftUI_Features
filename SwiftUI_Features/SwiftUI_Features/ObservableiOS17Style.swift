b2c5dd262ab7540e35d69db842b3988c1e03d852
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-03-27 11:13:24 +0200

    add new view for example how observable changed

---
+++ SwiftUI_Features/SwiftUI_Features/ObservableiOS17Style.swift
@@ -0,0 +1,18 @@
+//
+//  ObservableiOS17Style.swift
+//  SwiftUI_Features
+//
+//  Created by Oleksandr Isaiev on 27.03.2024.
+//
+
+import SwiftUI
+
+struct ObservableiOS17Style: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObservableiOS17Style()
+}
