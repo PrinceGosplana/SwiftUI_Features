6f0a703f68d40601a87f1b2c737fd81aee92590e
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-03-27 11:32:57 +0200

    add observable style before ios 17

--- SwiftUI_Features/SwiftUI_Features/ObservableiOS17Style.swift
+++ SwiftUI_Features/SwiftUI_Features/ObservableiOS17Style.swift
@@ -7,9 +7,42 @@
 
 import SwiftUI
 
+class UserObserve: ObservableObject {
+    @Published var name: String
+    @Published var jobTitle: String
+    @Published var followersCount: Int
+    @Published var bio: String
+
+    init(name: String, jobTitle: String, followersCount: Int, bio: String) {
+        self.name = name
+        self.jobTitle = jobTitle
+        self.followersCount = followersCount
+        self.bio = bio
+    }
+}
+
 struct ObservableiOS17Style: View {
+
+    @StateObject var user = UserObserve(name: "Awesome Curt",
+                                        jobTitle: "iOS Dev Streamer", followersCount: 120, 
+                                        bio: "My bio goes somewhere")
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            Text(user.name)
+                .font(.title.bold())
+            Text(user.jobTitle)
+                .foregroundStyle(.secondary)
+            Text("\(user.followersCount) followers")
+                .foregroundStyle(.secondary)
+
+            Button {
+                user.followersCount += 1
+            } label: {
+                Label("Add Follower", systemImage: "person.badge.plus")
+            }
+            .buttonStyle(.borderedProminent)
+            .padding(.top, 30)
+        }
     }
 }
 
