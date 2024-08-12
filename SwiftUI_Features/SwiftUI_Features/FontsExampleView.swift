fc86ec8c6ae6769d9fe5b1f5dbe163ec24b890cc
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-12 09:50:42 +0300

    update text size

--- SwiftUI_Features/SwiftUI_Features/FontsExampleView.swift
+++ SwiftUI_Features/SwiftUI_Features/FontsExampleView.swift
@@ -11,37 +11,37 @@ struct FontsExampleView: View {
     var body: some View {
         VStack(spacing: 30) {
 
-            Text("LargeTitle")
+            Text("LargeTitle - 34")
                 .font(.largeTitle)
 
-            Text("Title")
+            Text("Title - 28")
                 .font(.title)
 
-            Text("Title2")
+            Text("Title2 - 22")
                 .font(.title2)
 
-            Text("Title3")
+            Text("Title3 - 20")
                 .font(.title3)
 
-            Text("Headline")
+            Text("Headline - 17")
                 .font(.headline)
 
-            Text("Subheadline")
+            Text("Subheadline - 15")
                 .font(.subheadline)
 
-            Text("Body")
+            Text("Body - 17")
                 .font(.body)
 
-            Text("Callout")
+            Text("Callout - 16")
                 .font(.callout)
 
-            Text("Footnote")
+            Text("Footnote - 13")
                 .font(.footnote)
 
-            Text("Caption")
+            Text("Caption - 12")
                 .font(.caption)
 
-            Text("Caption2")
+            Text("Caption2 - 11")
                 .font(.caption2)
 
         }
