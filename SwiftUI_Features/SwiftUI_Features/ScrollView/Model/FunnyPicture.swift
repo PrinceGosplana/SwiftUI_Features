5a369061449e7f78a916e633d3a16f7e050e32ba
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-15 10:15:47 +0300

    add model

---
+++ SwiftUI_Features/SwiftUI_Features/ScrollView/Model/FunnyPicture.swift
@@ -0,0 +1,27 @@
+//
+//  FunnyPicture.swift
+//  SwiftUI_Features
+//
+//  Created by Oleksandr Isaiev on 15.08.2024.
+//
+
+import SwiftUI
+
+struct FunnyPicture: Identifiable {
+    let id = UUID()
+    let name: String
+    let image: String
+}
+
+extension FunnyPicture {
+    static let images: [FunnyPicture] = [
+        .init(name: "Bunny", image: "previewSample1"),
+        .init(name: "Pigeon", image: "previewSample2"),
+        .init(name: "Bug", image: "previewSample3"),
+        .init(name: "Cat", image: "previewSample4"),
+        .init(name: "Dog", image: "previewSample5"),
+        .init(name: "Rat", image: "previewSample6"),
+        .init(name: "Racoon", image: "previewSample7"),
+        .init(name: "Mouse", image: "previewSample8"),
+    ]
+}
