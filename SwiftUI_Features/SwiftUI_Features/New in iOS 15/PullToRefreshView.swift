c6f3fa109cc6869d644612cc622aeb5f6fd83cff
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-21 12:25:17 +0300

    pull to refresh

---
+++ SwiftUI_Features/SwiftUI_Features/New in iOS 15/PullToRefreshView.swift
@@ -0,0 +1,55 @@
+//
+//  PullToRefreshView.swift
+//  SwiftUI_Features
+//
+//  Created by Oleksandr Isaiev on 21.08.2024.
+//
+
+import SwiftUI
+
+struct PullToRefreshView: View {
+
+    let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+    @State var users: [PTRUser] = []
+
+    var body: some View {
+        NavigationStack {
+            List(users) { user in
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(user.name)
+
+                    Text(user.email)
+                        .font(.caption)
+                }
+            }
+            /// refresh control indicator will show until async task finished
+            .refreshable {
+                await fetchUsers()
+            }
+            .navigationTitle("Pull to Refresh")
+        }
+    }
+
+    // fetching user in async, must be in viewModel
+    private func fetchUsers() async {
+        let session = URLSession(configuration: .default)
+
+        do {
+            let task = try await session.data(from: url)
+            users = try JSONDecoder().decode([PTRUser].self, from: task.0)
+        } catch {
+            print("error")
+        }
+    }
+}
+
+#Preview {
+    PullToRefreshView()
+}
+
+
+struct PTRUser: Identifiable, Decodable {
+    let id: Int
+    let name: String
+    let email: String
+}
