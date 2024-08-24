b580e2ce5c35b99f24b87e6b1d479a0c3b2b5d23
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-08-24 12:27:35 +0300

    async image

---
+++ SwiftUI_Features/SwiftUI_Features/New in iOS 15/AsyncImageView.swift
@@ -0,0 +1,47 @@
+//
+//  AsyncImageView.swift
+//  SwiftUI_Features
+//
+//  Created by Oleksandr Isaiev on 24.08.2024.
+//
+
+import SwiftUI
+
+struct AsyncImageView: View {
+
+    let url = URL(string: "https://images.pexels.com/photos/163037/london-street-phone-cabin-163037.jpeg")!
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+                /*
+                AsyncImage(url: url)
+                /// Aspect ration isn't working directly...
+                /// only frame is working with embedded with clipped
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 600)
+                    .clipped()
+                 */
+                AsyncImage(url: url, scale: 1.0) { phase in
+                    if let image = phase.image {
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 300, height: 300)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                    } else {
+                        // Error or loading
+                        Image(systemName: "house.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.yellow)
+                    }
+                }
+            }
+            .navigationTitle("Async Image")
+        }
+    }
+}
+
+#Preview {
+    AsyncImageView()
+}
