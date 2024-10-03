6cef2c12ec65eb9b874a43155c6200e1f5d296b7
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-03 14:45:25 +0300

    time line animation

--- SwiftUI_Features/SwiftUI_Features/Animations/TimeLineAnimationView.swift
+++ SwiftUI_Features/SwiftUI_Features/Animations/TimeLineAnimationView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct TimeLineAnimationView: View {
 
-    @State private var isOn = false
 
     var body: some View {
 
@@ -23,18 +22,42 @@ struct TimeLineAnimationView: View {
 }
 
 struct ParticleModifier: ViewModifier {
+
+    @State private var startTime = Date.now
+    @State var particles: [Particle] = []
+
+    struct Particle: Codable {
+        var amplitude: CGFloat = .random(in: -50...50)
+        var delay: TimeInterval = .random(in: 0...2)
+    }
+
     func body(content: Content) -> some View {
         content
             .background {
-                Canvas { ctx, size in
-                    ctx.fill(Path(CGRect(origin: .zero, size: size)), with: .foreground)
-                    let heart = ctx.resolveSymbol(id: "particle")!
-//                    ctx.translateBy(x: size.width / 2, y: size.height / 2)
-                    ctx.draw(heart, at: .init(x: 50, y: 50))
-                } symbols: {
-                    content.tag("particle")
+                TimelineView(.animation) { timelineCtx in
+                    let elapsed = timelineCtx.date.timeIntervalSince(startTime)
+
+                    Canvas { ctx, size in
+                        ctx.fill(Path(CGRect(origin: .zero, size: size)), with: .foreground)
+                        let heart = ctx.resolveSymbol(id: "particle")!
+                        ctx.translateBy(x: size.width / 2, y: size.height / 2)
+                        for p in particles {
+                            let time = elapsed - p.delay
+                            guard time > 0 else { continue }
+                            let x = sin(elapsed * .pi) * p.amplitude
+                            let y = time * -30
+                            ctx.opacity = 1 - time / 4
+                            ctx.draw(heart, at: .init(x: x, y: y))
+                        }
+                    } symbols: {
+                        content.tag("particle")
+                    }
                 }
                 .frame(width: 300, height: 300)
+            }
+            .onTapGesture {
+                startTime = .now
+                particles = (0..<5).map { _ in Particle() }
             }
     }
 }
