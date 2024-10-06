d10ab9242443cd6c4c94195f88e7080b33fea9a7
OLEKSANDR ISAIEV <oleksandrisaiev@OLEKSANDRs-Mac-mini.local>
2024-10-06 10:00:07 +0300

    GridCellColumns

---
+++ SwiftUI_Features/SwiftUI_Features/Grids/GridCellColumnsExample.swift
@@ -0,0 +1,38 @@
+//
+//  GridCellColumnsExample.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 06.10.2024.
+//
+
+import SwiftUI
+
+struct GridCellColumnsExample: View {
+    var body: some View {
+        Grid {
+            GridRow {
+                Color.green.opacity(0.3)
+                    .gridCellColumns(3)
+            }
+            GridRow {
+                Color.cyan.opacity(0.3)
+                    .gridCellColumns(2)
+                Color.mint.opacity(0.3)
+            }
+            GridRow {
+                Color.red.opacity(0.3)
+                Color.orange.opacity(0.3)
+                Color.yellow.opacity(0.3)
+            }
+            GridRow {
+                Color.indigo.opacity(0.3)
+                Color.purple.opacity(0.3)
+                    .gridCellColumns(2)
+            }
+        }
+    }
+}
+
+#Preview {
+    GridCellColumnsExample()
+}
