2e5d0f9f3e6c4220a8da671a872f764974056a78
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-18 20:48:47 +0300

    start with Photo Grid view

---
+++ SwiftUI_Features/SwiftUI_Features/PhotoGridView.swift
@@ -0,0 +1,32 @@
+//
+//  PhotoGridView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 18.10.2024.
+//
+
+import SwiftUI
+
+struct PhotosView: View {
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: [.init(.adaptive(minimum: 100, maximum: .infinity), spacing: 3)]) {
+                ForEach(1..<8) { ix in
+                    Image("previewSample\(ix)")
+                }
+            }
+        }
+    }
+}
+
+
+struct PhotoGridView: View {
+    var body: some View {
+        PhotosView()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    PhotoGridView()
+}
