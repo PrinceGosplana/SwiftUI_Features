5a092ca8a2951e475da357ae40d3ce39b6abfffc
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-18 20:31:37 +0300

    BentoLayoutView

---
+++ SwiftUI_Features/SwiftUI_Features/BentoLayoutView.swift
@@ -0,0 +1,54 @@
+//
+//  BentoLayoutView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 18.10.2024.
+//
+
+import SwiftUI
+
+struct Split<Content: View>: View {
+    @Environment(\.direction) var axis
+    @ViewBuilder var content: Content
+    
+    var body: some View {
+        HStack {
+            let layout = axis == .horizontal ? AnyLayout(HStackLayout()) : AnyLayout(VStackLayout())
+            layout {
+                content
+            }
+            .environment(\.direction, axis.other)
+        }
+    }
+}
+
+extension Axis {
+    var other: Self {
+        self == .horizontal ? .vertical : .horizontal
+    }
+}
+
+extension EnvironmentValues {
+    @Entry var direction: Axis = .vertical
+}
+
+struct BentoLayoutView: View {
+    var body: some View {
+        Split {
+            Split {
+                Color.red
+                Split {
+                    Color.blue
+                    Color.yellow
+                }
+                Color.teal
+            }
+            Color.green
+            Color.cyan
+        }
+    }
+}
+
+#Preview {
+    BentoLayoutView()
+}
