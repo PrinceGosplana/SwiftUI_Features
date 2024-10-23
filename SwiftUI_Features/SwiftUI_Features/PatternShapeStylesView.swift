68d806fac9ed7cff1ba17984c4e90cbb1479d4c0
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-23 18:51:29 +0300

    add new struct

--- SwiftUI_Features/SwiftUI_Features/PatternShapeStylesView.swift
+++ SwiftUI_Features/SwiftUI_Features/PatternShapeStylesView.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+struct HLine: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 1)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
 struct PatternShapeStylesView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
