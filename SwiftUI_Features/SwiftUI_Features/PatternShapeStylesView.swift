7b214dad50dc3d56d053d9230b396c84e648e697
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-23 18:50:37 +0300

    Pattern Shape Styles

---
+++ SwiftUI_Features/SwiftUI_Features/PatternShapeStylesView.swift
@@ -0,0 +1,18 @@
+//
+//  PatternShapeStylesView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 23.10.2024.
+//
+
+import SwiftUI
+
+struct PatternShapeStylesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PatternShapeStylesView()
+}
