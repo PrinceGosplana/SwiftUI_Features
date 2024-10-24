f6ddafcb5be873e895cd76e7122cb2ec40bb5218
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-24 20:39:49 +0300

    expand struct

--- SwiftUI_Features/SwiftUI_Features/CubicBezieKeyframesView.swift
+++ SwiftUI_Features/SwiftUI_Features/CubicBezieKeyframesView.swift
@@ -9,6 +9,15 @@ import SwiftUI
 
 struct CubicBezier<Value: VectorArithmetic> {
     var p0, p1, p2, p3: Value
+
+    func value(for t: Double) -> Value {
+        assert(t >= 0 && t <= 1)
+
+        return pow(1 - t, 3) * p0
+        + 3 * pow(1 - t, 2) * t * p1
+        + 3 * (1 - t) * pow(t, 2) * p2
+        + pow(t, 3) * p3
+    }
 }
 
 struct CubicBezieKeyframesView: View {
