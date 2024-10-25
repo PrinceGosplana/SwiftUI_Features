f76bd646ea469b0abec87d0b6c442ebd3e58e071
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-25 19:15:08 +0300

    Animating with Cubic Keyframes

--- SwiftUI_Features/SwiftUI_Features/CubicBezieKeyframesView.swift
+++ SwiftUI_Features/SwiftUI_Features/CubicBezieKeyframesView.swift
@@ -8,74 +8,27 @@
 import SwiftUI
 import Charts
 
-struct CubicBezier<Value: VectorArithmetic> {
-    var p0, p1, p2, p3: Value
-
-    func value(for t: Double) -> Value {
-        assert(t >= 0 && t <= 1)
-
-        return pow(1 - t, 3) * p0
-        + 3 * pow(1 - t, 2) * t * p1
-        + 3 * (1 - t) * pow(t, 2) * p2
-        + pow(t, 3) * p3
-    }
-}
-
 extension VectorArithmetic {
     static func *(lhs: Double, rhs: Self) -> Self {
         rhs.scaled(by: lhs)
     }
 }
 
-struct CubicBezieKeyframesView: View {
-    var body: some View {
-        let c = CubicBezier<AnimatablePair>(p0: .init(0, 0), p1: .init(1, 0), p2: .init(2/3.0, 1), p3: .init(1, 1))
-        
-        let u = UnitCurve.bezier(startControlPoint: .init(x: 1, y: 0), endControlPoint: .init(x: 2/3.0, y: 1))
-        let xs = Array(stride(from: 0, through: 1, by: 0.01))
-        Chart {
-            ForEach(xs, id: \.self) { t in
-                let p = c.value(for: t)
-                LineMark(x: .value("x", Double(p.first)), y: .value("y", Double(p.second)), series: .value("1", "1"))
-            }
-            ForEach(xs, id: \.self) { x in
-                LineMark(x: .value("x", Double(x)), y: .value("y", Double(u.value(at: x))), series: .value("2", "2"))
-                    .foregroundStyle(.green)
-            }
-        }
+struct CubicBezier<Value: VectorArithmetic> {
+    var p0, p1, p2, p3: Value
+    
+    func value(for t: Double) -> Value {
+        assert(t >= 0 && t <= 1)
+        return pow(1-t, 3) * p0 +
+        3 * pow(1-t, 2) * t * p1 +
+        3 * (1-t) * pow(t, 2) * p2 +
+        pow(t, 3) * p3
     }
 }
 
-extension CubicBezier where Value == Double {
+extension CubicBezier {
     func map<V: VectorArithmetic>(transform: (Value) -> V) -> CubicBezier<V> {
-            CubicBezier<V>(
-                p0: transform(p0),
-                p1: transform(p1),
-                p2: transform(p2),
-                p3: transform(p3)
-            )
-        }
-}
-
-extension CGPoint: VectorArithmetic {
-    public mutating func scale(by rhs: Double) {
-        animatableData.scale(by: rhs)
-    }
-    
-    public var magnitudeSquared: Double {
-        animatableData.magnitudeSquared
-    }
-    
-    public static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
-        var copy = lhs
-        copy.animatableData += rhs.animatableData
-        return copy
-    }
-    
-    public static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
-        var copy = lhs
-        copy.animatableData -= rhs.animatableData
-        return copy
+        CubicBezier<V>(p0: transform(p0), p1: transform(p1), p2: transform(p2), p3: transform(p3))
     }
 }
 
@@ -96,6 +49,49 @@ extension CubicBezier where Value == Double {
             result = value(for: mid)
         } while abs(result-time) > epsilon
         return mid
+    }
+}
+
+extension CGPoint: VectorArithmetic {
+    public static func - (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        var copy = lhs
+        copy.animatableData -= rhs
+        return copy
+    }
+    
+    public mutating func scale(by rhs: Double) {
+        animatableData.scale(by: rhs)
+    }
+    
+    public var magnitudeSquared: Double {
+        animatableData.magnitudeSquared
+    }
+    
+    public static func +(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        var copy = lhs
+        copy.animatableData += rhs.animatableData
+        return copy
+    }
+    
+}
+
+struct CubicBezieKeyframesView: View {
+    var body: some View {
+        let c = CubicBezier<AnimatablePair>(p0: .init(0, 0), p1: .init(0.5, 0), p2: .init(2/3.0, 1), p3: .init(1, 1))
+        let timeBezier = c.map { $0.first }
+        let u = UnitCurve.bezier(startControlPoint: .init(x: 0.5, y: 0), endControlPoint: .init(x: 2/3.0, y: 1))
+        let xs = Array(stride(from: 0, through: 1, by: 0.01))
+        Chart {
+            ForEach(xs, id: \.self) { x in
+                let t = timeBezier.value(for: x)
+                let p = c.value(for: t)
+                LineMark(x: .value("x", Double(p.first)), y: .value("y", Double(p.second)), series: .value("1", "1"))
+            }
+            ForEach(xs, id: \.self) { x in
+                LineMark(x: .value("x", Double(x)), y: .value("y", Double(u.value(at: x))), series: .value("2", "2"))
+                    .foregroundStyle(.green)
+            }
+        }
     }
 }
 
