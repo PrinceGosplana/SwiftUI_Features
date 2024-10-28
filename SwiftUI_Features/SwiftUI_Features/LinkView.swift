d7300401fcab03f1e6b1e2a59569bcbd94b19863
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-28 21:10:44 +0200

    link example

---
+++ SwiftUI_Features/SwiftUI_Features/LinkView.swift
@@ -0,0 +1,18 @@
+//
+//  LinkView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 28.10.2024.
+//
+
+import SwiftUI
+
+struct LinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LinkView()
+}
