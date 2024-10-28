985b1679cbd62b312f7af722993d04641a79b5f9
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-28 21:15:33 +0200

    StaticListView

---
+++ SwiftUI_Features/SwiftUI_Features/StaticListView.swift
@@ -0,0 +1,18 @@
+//
+//  StaticListView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 28.10.2024.
+//
+
+import SwiftUI
+
+struct StaticListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StaticListView()
+}
