00c8f0418054089f26ac4d1b4b9dc116e929f8da
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-28 21:11:50 +0200

    custom link

--- SwiftUI_Features/SwiftUI_Features/LinkView.swift
+++ SwiftUI_Features/SwiftUI_Features/LinkView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct LinkView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Link("Go to Apple", destination: URL(string: "https://www.apple.com")!)
+            
+            Link(destination: URL(string: "https://www.apple.com")!) {
+                HStack(spacing: 20.0) {
+                    Image(systemName: "applelogo")
+                    Text("Go to Apple")
+                }
+                .foregroundStyle(.white)
+                .padding()
+                .padding(.horizontal)
+                .background(Capsule()
+                    .fill(Color.blue)
+                    .shadow(radius: 8, y: 18))
+            }
+        }
     }
 }
 
