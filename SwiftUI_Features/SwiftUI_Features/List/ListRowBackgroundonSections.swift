8ef2f10d1ad97ea1b14efd6a1237c9a6a79fa85d
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-29 20:39:33 +0200

    ListRowBackgroundonSections

---
+++ SwiftUI_Features/SwiftUI_Features/List/ListRowBackgroundonSections.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowBackgroundonSections.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 29.10.2024.
+//
+
+import SwiftUI
+
+struct ListRowBackgroundonSections: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowBackgroundonSections()
+}
