fc879e7f3b2a06f80ad26ed2daccac54a96831f0
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-29 20:39:41 +0200

    ListRowBackgroundonSections

--- SwiftUI_Features/SwiftUI_Features/List/ListRowBackgroundonSections.swift
+++ SwiftUI_Features/SwiftUI_Features/List/ListRowBackgroundonSections.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct ListRowBackgroundonSections: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            //            There is no modifier for a section
+            //                                        background but you can use the
+            //                                        listRowBackground modifier to
+            //                                        change the section’s color.
+            Section("Section 1") {
+                Text("Item 1")
+                Text("Item 2")
+                Text("Item 3")
+            }
+            .listRowBackground(Color.green.opacity(0.2))
+            //            Use width: 214
+            Section("Section 2") {
+                Text("Item 1")
+                Text("Item 2")
+                Text("Item 3")
+            }
+            .listRowBackground(Color.blue.opacity(0.2))
+        }
+        .headerProminence(.increased)
     }
 }
 
