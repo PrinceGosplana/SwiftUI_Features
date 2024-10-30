a7994aa1e9635570b129a6994b89c2914ad96078
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-10-30 21:47:59 +0200

    ListSafeAreaInsetScrollView

---
+++ SwiftUI_Features/SwiftUI_Features/List/ListSafeAreaInsetScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  ListSafeAreaInsetScrollView.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 30.10.2024.
+//
+
+import SwiftUI
+
+struct ListSafeAreaInsetScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListSafeAreaInsetScrollView()
+}
