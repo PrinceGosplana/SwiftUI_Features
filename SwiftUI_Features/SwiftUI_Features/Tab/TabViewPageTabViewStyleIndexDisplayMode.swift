a0ab95add5790c04ea8cca1f49d1162a6b86255a
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-11-19 19:29:56 +0200

    TabViewPageTabViewStyleIndexDisplayMode

--- SwiftUI_Features/SwiftUI_Features/Tab/TabViewPageTabViewStyleIndexDisplayMode.swift
+++ SwiftUI_Features/SwiftUI_Features/Tab/TabViewPageTabViewStyleIndexDisplayMode.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct TabViewPageTabViewStyleIndexDisplayMode: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            VStack(spacing: 20) {
+                
+                Text("PAGE 1")
+                Spacer()
+                
+            }
+            VStack {
+                
+                Text("PAGE 2")
+            }
+            VStack {
+                Text("PAGE 3")
+            }
+        }
+        .font(.title)
+        // hide dots
+        .tabViewStyle(.page(indexDisplayMode: .never))
     }
 }
 
