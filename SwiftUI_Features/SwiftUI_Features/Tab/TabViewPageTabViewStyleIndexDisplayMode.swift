566ed35163cceab08f8b3484179170cafecf6d73
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-11-19 19:29:49 +0200

    TabViewPageTabViewStyleIndexDisplayMode

---
+++ SwiftUI_Features/SwiftUI_Features/Tab/TabViewPageTabViewStyleIndexDisplayMode.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewPageTabViewStyleIndexDisplayMode.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 19.11.2024.
+//
+
+import SwiftUI
+
+struct TabViewPageTabViewStyleIndexDisplayMode: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabViewPageTabViewStyleIndexDisplayMode()
+}
