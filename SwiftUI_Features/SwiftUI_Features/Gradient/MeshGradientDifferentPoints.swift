7b5be4ba1a80d327f4fdd10302a34dd2e461ceaf
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-12-29 21:28:38 +0200

    MeshGradientDifferentPoints

---
+++ SwiftUI_Features/SwiftUI_Features/Gradient/MeshGradientDifferentPoints.swift
@@ -0,0 +1,21 @@
+//
+//  MeshGradientDifferentPoints.swift
+//  SwiftUI_Features
+//
+//  Created by OLEKSANDR ISAIEV on 29.12.2024.
+//
+
+import SwiftUI
+
+struct MeshGradientDifferentPoints: View {
+    var body: some View {
+        if #available(iOS 18.0, *) {
+            MeshGradient(width: 2, height: 2, points: [[0,0], [1, 0], [0.5, 0], [1,1]], colors: [.red, .blue, .yellow, .purple])
+                .ignoresSafeArea()
+        }
+    }
+}
+
+#Preview {
+    MeshGradientDifferentPoints()
+}
